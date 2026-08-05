383e3637252d97cc2578681333ef4b1ff7ebce67
MiszterSoul <jerrysoul921@gmail.com>
2026-08-05 19:43:21 +0200

    Refresh positioning guide cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pd-meter-v5';
+const CACHE='pd-meter-v6';
 const ASSETS=['./','./index.html','./style.css','./app.js','./card-worker.js','./manifest.json'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
