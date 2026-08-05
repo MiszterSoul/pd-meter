dafa98553853f81e82d1f86924e82320fda56b71
MiszterSoul <jerrysoul921@gmail.com>
2026-08-05 19:20:26 +0200

    Refresh offline cache

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE='pd-meter-v1';
+const CACHE='pd-meter-v2';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
 self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET') return;
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request)));
 });
