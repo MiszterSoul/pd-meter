f079118e3398fdd6d1a112e61e1437c602ab41aa
MiszterSoul <jerrysoul921@gmail.com>
2026-08-05 19:38:15 +0200

    Cache lightweight card detector

--- sw.js
+++ sw.js
@@ -1,8 +1,5 @@
-const CACHE='pd-meter-v4';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json'];
+const CACHE='pd-meter-v5';
+const ASSETS=['./','./index.html','./style.css','./app.js','./card-worker.js','./manifest.json'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET')return;
-  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request)));
-});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request)));});
